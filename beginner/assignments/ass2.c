61913c1aa7ab3c64ba4d25b7785141b4727f6270
POOJAKRISHNAMOORTHI <34641384+POOJAKRISHNAMOORTHI@users.noreply.github.com>
2017-12-18 09:29:57 +0000

    Add files via upload

---
+++ beginner/assignments/ass2.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+char a[50]="sachin ramesh tendulkar";
+char b[30]="Little Master";
+int c=18426;
+float d=44.83;
+int e=463;
+printf("Name is %s\n",a);
+printf("Nick Name is %s\n",b);
+printf("Runs Scored in ODI is %d\n",c);
+printf("Bating average in ODI is %.2f\n",d);
+printf("ODI Matches %d\n",e);
+return 0;
+}
+
