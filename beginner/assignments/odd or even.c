ec48fe80006bbbdbdd54fe5bf24d389f6e5cdd6c
POOJAKRISHNAMOORTHI <34641384+POOJAKRISHNAMOORTHI@users.noreply.github.com>
2017-12-19 05:28:24 +0000

    Add files via upload

---
+++ beginner/assignments/odd or even.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+int num;
+printf("enter the number:\n");
+scanf("%d",&num);
+if(n%2==0)
+{
+printf("the number is even");
+}
+else
+{
+printf("the number is odd");
+}
+return 0;
+}
+
---
+++ beginner/assignments/positive or negative.c
@@ -0,0 +1,19 @@
+1.
+#include<stdio.h>
+int main()
+{
+int num;
+printf("enter the number:\n");
+scanf("%d",&num);
+if(num>0)
+{
+printf("the number is positive");
+}
+else
+{
+printf("the number is negative");
+}
+return 0;
+}
+
+
