a78693678894395fb6f68b1531f00f268763165d
POOJAKRISHNAMOORTHI <34641384+POOJAKRISHNAMOORTHI@users.noreply.github.com>
2018-01-04 23:00:53 +0530

    Add files via upload

---
+++ beginner/assignments/sum of digits of a given number.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main(void) 
+
+{
+	
+int i,a[100],n,k,sum=0;
+	
+scanf("%d",&n);
+	
+scanf("%d",&k);
+	
+for(i=0;i<n;i++)
+	
+{
+		
+scanf("%d",&a[i]);
+	
+}
+	
+	
+for(i=0;i<k;i++)
+	
+{
+    
+sum=sum+a[i];
+    
+}
+    
+printf("the sum is %d",sum);
+	
+return 0;
+
+}
