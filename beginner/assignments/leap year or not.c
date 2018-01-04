a6587309ae937861cc62b4f828376a0d5df62951
POOJAKRISHNAMOORTHI <34641384+POOJAKRISHNAMOORTHI@users.noreply.github.com>
2018-01-04 22:20:34 +0530

    Add files via upload

---
+++ beginner/assignments/leap year or not.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+
+int main(void) 
+
+{
+int year;
+	
+printf("enter the year:\n");
+	
+scanf("%d",&year);
+	
+if (((year % 4 == 0) && (year % 100!= 0)) || (year%400 == 0))
+	
+{
+            
+printf("%d is a leap year", year);
+	
+}
+   
+           
+else
+            
+{
+           
+printf("%d is a notleap year", year);
+	
+}
+  
+	
+return 0;
+
+}
