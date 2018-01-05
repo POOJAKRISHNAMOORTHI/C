3e9ce0ad7a7317fea3f1c4cb86dc53fa1fcf05db
POOJAKRISHNAMOORTHI <34641384+POOJAKRISHNAMOORTHI@users.noreply.github.com>
2018-01-05 22:19:23 +0530

    Add files via upload

---
+++ beginner/assignments/palindrome or not.c
@@ -0,0 +1,36 @@
+
+#include<stdio.h>
+
+int main()
+ 
+{
+	
+int n,x,rev=0,temp;
+	
+scanf("%d",&n);
+	
+temp=n;
+	
+while(n!=0)
+	
+{
+		
+x=n%10;
+		
+rev=rev*10+x;
+		
+n=n/10;
+	
+}
+	
+printf("reverse of a number is %d\n",rev);
+	
+if(temp==rev)
+	
+printf("yes");
+	
+else
+	
+printf("No");
+
+}
