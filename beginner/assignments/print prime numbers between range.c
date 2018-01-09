1579ae426a7983877186f01687c8eb1a0e54841f
POOJAKRISHNAMOORTHI <34641384+POOJAKRISHNAMOORTHI@users.noreply.github.com>
2018-01-09 22:13:38 +0530

    Update print prime numbers between range.c

--- beginner/assignments/print prime numbers between range.c
+++ beginner/assignments/print prime numbers between range.c
@@ -1,17 +1,18 @@
 #include<stdio.h>
-int main(void)
+int main(void) 
 {
-    int n1,n2,count,i,j;
-    scanf("%d %d",&n1,&n2);
-    for(j=n1;j<=n2;j++)
-    {
-       count=0;
-       for(i=1;i<=j;i++)
-       {
-          if(j%i==0)   
-             count++;
-       }
-       if(count==2)
-          printf("%d\t",j);
-    }
+  int i,j,start,end,count;
+  scanf("%d %d",&start,&end);
+  for(j=start;j<=end;j++)
+  {
+         count=0;
+         for(j=1;j<=i;j++)
+         {
+	if(i%j==0)
+	 count++;
+	
+         }
+         if(count==2)
+          printf("%d\t",i);
+  }
 }
