13fb367da11e3808584734ce328ba6d0a47da560
POOJAKRISHNAMOORTHI <34641384+POOJAKRISHNAMOORTHI@users.noreply.github.com>
2018-01-25 10:06:03 +0530

    Create swap even and odd character in string.c

---
+++ practice/swap even and odd character in string.c
@@ -0,0 +1,19 @@
+
+#include <stdio.h>
+#include<string.h>
+int main(void) 
+{
+    char a[40],temp;
+    int l,i;
+    printf("enter the string:\n");
+    scanf("%s",a);
+    l=strlen(a);
+    for(i=0;i<=l;i=i+2)
+    {
+    	temp=a[i];
+    	a[i]=a[i+1];
+    	a[i+1]=temp;
+    }
+    printf("string is %s",a);
+	return 0;
+}
