f72f44f72540daeffc58837beef3a4a3e47c57bd
POOJAKRISHNAMOORTHI <34641384+POOJAKRISHNAMOORTHI@users.noreply.github.com>
2018-01-25 10:03:53 +0530

    Create rotating an array.c

---
+++ practice/rotating an array.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main(void) 
+{
+	int arr[100],i=0,j,temp,n,p;
+	printf("enter the num:\n");
+	scanf("%d",&n);
+	printf("enter the num of rotation:\n");
+	scanf("%d",&p);
+	printf("enter the array:\n" );
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&arr[i]);
+	}
+		
+	for(i=1;i<=p;i++)
+	{
+		temp=arr[0];
+		for(j=0;j<n-1;j++)
+		{
+			arr[j]=arr[j+1];
+		}
+		arr[n-1]=temp;
+	}
+	for(i=0;i<n;i++)
+	{
+	printf("%d",arr[i]);
+	}
+	return 0;
+}
