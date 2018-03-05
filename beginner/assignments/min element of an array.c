3246c56062f6c7f1728232e48121761a98acd756
POOJAKRISHNAMOORTHI <34641384+POOJAKRISHNAMOORTHI@users.noreply.github.com>
2018-03-05 12:57:59 +0530

    Create min element of an array.c

---
+++ beginner/assignments/min element of an array.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main()
+{
+	int i,a[10],n,temp,j,max;
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	for(i=0;i<n-1;i++)
+	{
+		for(j=0;j<n-i-1;j++)
+		{
+			if(a[j]<a[j+1])
+			{
+				temp=a[j];
+				a[j]=a[j+1];
+				a[j+1]=temp;
+			}
+		}
+	
+	
+	}
+	max=a[0];
+	printf("%d",max);
+	
+	return 0;
+}
