06e7dc91f79c513a2cf281dec5d8947cadb5a1ef
POOJAKRISHNAMOORTHI <34641384+POOJAKRISHNAMOORTHI@users.noreply.github.com>
2018-06-22 15:25:00 +0530

    Update calculator for div and mod.c

--- beginner/assignments/set 9/calculator for div and mod.c
+++ beginner/assignments/set 9/calculator for div and mod.c
@@ -3,11 +3,11 @@
 int main(void) 
 {
 	int a,b,i;
-	char op;
+	char oper;
 	for(i=0;i<4;i++)
 	{
-	scanf("%d %c %d",&a,&op,&b);
-	if(op=='/')
+	scanf("%d %c %d",&a,&oper,&b);
+	if(oper=='/')
 	printf("%d\n",a/b);
 	else
 	printf("%d\n",a%b);
